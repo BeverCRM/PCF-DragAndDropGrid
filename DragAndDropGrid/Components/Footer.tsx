987b96a34a7d076f17ab3d704ce8c20709456a44
Menua Sepoyan <83786471+menuasepoyan@users.noreply.github.com>
2022-08-30 01:28:14 +0400

    Feature/drag and drop grid refactoring (#1)
    * Initial version of DragAndDropGrid PCF Control

---
+++ DragAndDropGrid/Components/Footer.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { IconButton } from '@fluentui/react/lib/Button';
+import { usePagination } from './Pagination';
+import { footerStyles } from '../Styles/FooterStyles';
+
+type DataSet = ComponentFramework.PropertyTypes.DataSet;
+
+export interface IGridFooterProps {
+    dataset: DataSet;
+    selectedCount: number;
+}
+
+export const GridFooter = ({ dataset, selectedCount } : IGridFooterProps) => {
+  const {
+    currentPage,
+    firstItemNumber,
+    lastItemNumber,
+    totalRecords,
+    moveToFirst,
+    movePrevious,
+    moveNext,
+  } = usePagination(dataset);
+
+  const selected = `${firstItemNumber} - ${lastItemNumber}
+  of ${totalRecords} (${selectedCount} selected)`;
+  const page = `Page ${currentPage}`;
+
+  return <div>
+    <div className={footerStyles.content}>
+      <div className={footerStyles.footer}>
+        <div className={footerStyles.left}> {selected} </div>
+        <div className={footerStyles.right}>
+          <IconButton className={footerStyles.icon} iconProps={{ iconName: 'Previous' }}
+            onClick={moveToFirst} disabled={!dataset.paging.hasPreviousPage}/>
+          <IconButton className={footerStyles.icon} iconProps={{ iconName: 'Back' }}
+            onClick={movePrevious} disabled={!dataset.paging.hasPreviousPage}/>
+          <span> {page} </span>
+          <IconButton className={footerStyles.icon} iconProps={{ iconName: 'Forward' }}
+            onClick={moveNext} disabled={!dataset.paging.hasNextPage}/>
+        </div>
+      </div>
+    </div>
+  </div>;
+};
---
+++ DragAndDropGrid/Services/DataverseService.ts
@@ -0,0 +1,114 @@
+import { arrayBufferToBase64, readFileAsync } from '../Utils/Utils';
+import { IInputs } from '../generated/ManifestTypes';
+
+let _context: ComponentFramework.Context<IInputs>;
+
+export default {
+  setContext(context: ComponentFramework.Context<IInputs>) {
+    _context = context;
+  },
+
+  async getRecordRelatedNotes(targetEntityId: string) {
+    const targetEntityType: string = _context.parameters.dataset.getTargetEntityType();
+    let fetchXml: string = `
+    <fetch version="1.0" output-format="xml-platform" mapping="logical" distinct="false">
+      <entity name="annotation">
+        <attribute name="subject" />
+        <attribute name="notetext" />
+        <attribute name="filename" />
+        <attribute name="documentbody" />
+        <attribute name="mimetype" />
+        <attribute name="annotationid" />
+        <order attribute="subject" descending="false" />
+        <link-entity name="${targetEntityType}" from="${targetEntityType}id"
+        to="objectid" link-type="inner" alias="ac">
+          <filter type="and">
+            <condition attribute="${targetEntityType}id" operator="eq" value="${targetEntityId}" />
+          </filter>
+        </link-entity>
+      </entity>
+    </fetch>`;
+
+    fetchXml = `?fetchXml=${encodeURIComponent(fetchXml)}`;
+    const recordRelatedNotes = await _context.webAPI.retrieveMultipleRecords('annotation',
+      fetchXml);
+
+    return recordRelatedNotes;
+  },
+
+  async uploadFiles(files: FileList | undefined, targetEntityId: string): Promise<void> {
+    const targetEntityType: string = _context.parameters.dataset?.getTargetEntityType();
+    const entityMetadata = await _context.utils.getEntityMetadata(targetEntityType);
+
+    if (files !== undefined) {
+      for (let i = 0; i < files.length; i++) {
+        const file: File = files[i];
+        const buffer: ArrayBuffer = await readFileAsync(file);
+        const body: string = arrayBufferToBase64(buffer);
+
+        const note: any = {
+          filename: file.name,
+          subject: '',
+          documentbody: body,
+          mimetype: file.type,
+        };
+
+        note[`objectid_${targetEntityType}@odata.bind`] =
+        `/${entityMetadata._entitySetName}(${targetEntityId})`;
+        _context.webAPI.createRecord('annotation', note);
+      }
+    }
+  },
+
+  async deleteSelectedNotes(noteIds: string[]): Promise<void> {
+    try {
+      for (const id of noteIds) {
+        await _context.webAPI.deleteRecord('annotation', id);
+      }
+    }
+    catch (e) {
+      console.log(e);
+    }
+  },
+
+  deleteSelectedRecords(recordIds: string[]): void {
+    const targetEntityType: string = _context.parameters.dataset.getTargetEntityType();
+    try {
+      for (const id of recordIds) {
+        _context.webAPI.deleteRecord(targetEntityType, id);
+      }
+    }
+    catch (e) {
+      console.log(e);
+    }
+  },
+
+  openRecordCreateForm(): void {
+    const targetEntityType: string = _context.parameters.dataset.getTargetEntityType();
+    const entityFormOptions = {
+      entityName: targetEntityType,
+    };
+    _context.navigation.openForm(entityFormOptions).then(
+      (success: any) => {
+        console.log(success);
+      },
+      (error: any) => {
+        console.log(error);
+      });
+  },
+
+  onCalloutItemInvoked(item: any): void {
+    const entityFormOptions = {
+      entityName: 'annotation',
+      entityId: item.key,
+    };
+
+    _context.navigation.openForm(entityFormOptions).then(
+      (success: any) => {
+        console.log(success);
+      },
+      (error: any) => {
+        console.log(error);
+      });
+  },
+};
